fbcae64e7e4e1f59c79ed852e2c8a60fc7853b23
Diego-Zerwes <difoz2003@hotmail.com>
2024-05-03 22:39:06 -0300

    testando

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -6,3 +6,23 @@ $(window).on('scroll', function () {
       $("nav").removeClass('colored');
   }
 })
+
+function mostraSub(sub,protogeo){
+  subfilho = document.getElementById(sub,protogeo);
+  subfilho.style.display = 'block';
+}
+
+function escondeSub(sub,protogeo){
+  subfilho = document.getElementById(sub,protogeo);
+  subfilho.style.display = 'none'; 
+}
+
+function mostraNavBar(navBar){
+  subfilho = document.getElementById(navBar);
+  subfilho.style.display = 'block';
+}
+
+function esconderNavBar(navBar){
+  subfilho = document.getElementById(navBar);
+  subfilho.style.display = 'none'; 
+}
